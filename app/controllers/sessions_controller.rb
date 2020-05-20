b52c222cb63d7490ba7eef91580ef477125564e4
pharia-le <pharia.le@gmail.com>
2020-05-20 15:43:22 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+    def new
+    end
+    def create
+        if params[:name].nil? || params[:name].empty?
+            redirect_to "/sessions/new"
+        else
+            session[:name] = params[:name]
+        end
+    end
+
+    def destroy
+        session.clear if !session[:name].nil?
+    end
+end
